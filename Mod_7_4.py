6b8c1a5eb306f2fdfd59ed66fbcaf7c98afb6331
Grzegorz Wasilko <wasilko1@o2.pl>
2021-08-31 22:14:20 +0200

    Mixin functions..type movie error

--- Mod_7_4.py
+++ Mod_7_4.py
@@ -3,17 +3,30 @@ import logging
 logging.basicConfig(level=logging.DEBUG,format='%(asctime)s',filename="logfile-runningcount.log")
 from faker import Faker
 fake=Faker()
-#___________________________________________________________________________________________
+#********************************************************************************************
 
 #_____Base_class___________________class__Movie_________________________________Klasa Bazowa
 class Movie :
     def __init__(self,title,publication_date,movie_type,):#numb_of_play
-        self.title=title                                         #_________________Tytuł__fake.words(nb=3)
-        self.publication_date=publication_date                   #_________Rok wydania__<<-->> fake.date()
-        self.movie_type=movie_type                               #___________________Gatunek
+        self.title=title                            #____Tytuł________<<-->>_fake.words(nb=3)
+        self.publication_date=publication_date      #____Rok wydania__<<-->>______fake.date()
+        self.movie_type=movie_type                  #____Gatunek______<<-->>_fake.word(ext_word_list=(['Action','Musical','Historic'])/fake.random_int(min=0, max=15)
+#__________________________________Methods____________________________________________________
+    def __str__(self):
+        return (f'Tytuł: {self.title}, gatunek: {self.movie_type}, premiera: {self.publication_date}')
+    @property
+    def movie_type (self):
+        return self.movie_type
+    @movie_type.setter
+    def movie_type (self,value):
+        type=['Action','Musical','Historic']
+        _type=int(value)
+        self.movie_type =type[_type]
 
-#        self.numb_of_play=numb_of_play#__________Liczba odtworzeń_______TODO<<<<<<<<<<<<<<<<<<<<
-#metoda play---------_TODO
+# metoda self.numb_of_play=numb_of_play#__________Liczba odtworzeń___
+# metoda play---------_TODO_________________________ziekszanie odtworzen o 1 
+
+
 
 #_____Child_class__________________class__Series_______________________________Klasa Potomna
 class Series(Movie):
@@ -24,12 +37,14 @@ class Series(Movie):
         
 #        self.numb_of_play=numb_of_play#__________Liczba odtworzeń_______TODO<<<<<<<<<<<<<<<<<<<<
 #metoda play---------_TODO
+#def __str__(self)
 
     
-
+#**********************************************************************************************
 if __name__ == "__main__":
     logging.info("uruhomiono :")
     print("Bilioteka filmów")
-#___________tytuł____________rok_wydania__gatunek__
-film=Movie(fake.words(nb=3),fake.date(),'dramat')
+#______________tytuł________rok_wydania___gatunek__nr gatunku z lity w lini 22
+film=Movie(fake.words(nb=3),fake.date(),fake.random_int(min=0, max=2))#_______fake.int_to arg dla nr gatunku w property.setter le jest int zamiast str
 print(film.title)
+print(film)
